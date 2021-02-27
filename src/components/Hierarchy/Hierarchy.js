48f08f1447e94d7cfdab9961defeaecacd51872e
Shir Zabolotny <40990488+shir0206@users.noreply.github.com>
2021-02-27 10:09:32 +0200

    Export poision to constants

--- src/components/Hierarchy/Hierarchy.js
+++ src/components/Hierarchy/Hierarchy.js
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
 
 import "./Hierarchy.css";
 
 export const Hierarchy = (props) => {
+  let history = useHistory();
+
+  const navigateToLogIn = () => {
+    console.log("navigateToLogIn");
+    history.push("/LogIn");
+  };
+
+  function handleLogOut(navigateToLogIn) {
+    if (props.state.currLoggedUserID) {
+      //props.state.logOut(navigateToLogIn);
+      // navigateToLogIn();
+    }
+  }
+
   return (
     <div className="hierarchy-container">
       <h1>hierarchy</h1>
+      <h1>{props.state.currLoggedUserID}</h1>
+      <button onClick={handleLogOut(navigateToLogIn)}>Log out</button>
     </div>
   );
 };
